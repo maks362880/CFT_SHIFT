5c19ae1455625c4ccd51e7e7ba86d11f911014b8
maks362880 <48396251+maks362880@users.noreply.github.com>
2023-01-12 15:36:55 +0300

    --minor updates

--- src/controller/MergeSortNumbers.java
+++ src/controller/MergeSortNumbers.java
@@ -1,6 +1,14 @@
 package controller;
 
+import exeption_handling.ExceptionWrongParam;
+
 public class MergeSortNumbers implements MergeSort {
     public MergeSortNumbers(String[] args, int MAX_PART_SIZE_FILE) {
+
+        switch (args[1]) {
+            case ("-a") -> ;
+            case ("-d") -> ;
+            default -> ;
+        }
     }
 }
--- src/controller/MergeSortString.java
+++ src/controller/MergeSortString.java
@@ -2,5 +2,10 @@ package controller;
 
 public class MergeSortString implements MergeSort{
     public MergeSortString(String[] args, int MAX_PART_SIZE_FILE) {
+        switch (args[1]) {
+            case ("-a") -> ;
+            case ("-d") -> ;
+            default -> ;
+        }
     }
 }
