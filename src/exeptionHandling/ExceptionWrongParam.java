2e7195fd7ff3f430020dd279b9b6201947d050aa
maks362880 <48396251+maks362880@users.noreply.github.com>
2023-01-13 16:26:25 +0300

    - add logged error in 'log.txt' files

--- src/exeptionHandling/ExceptionWrongParam.java
+++ src/exeptionHandling/ExceptionWrongParam.java
@@ -1,8 +1,14 @@
 package exeptionHandling;
 
+import readAndWrite.WritePartOfStringFiles;
+
+import java.util.List;
+
 public class ExceptionWrongParam{
     public ExceptionWrongParam(String message) {
         System.out.println(message);
         //добавить логирование в файл
+        WritePartOfStringFiles writeString = new WritePartOfStringFiles();
+        writeString.write("log.txt", List.of(message));
     }
 }
