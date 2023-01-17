6db8757895889f2d54500ff53f655b2506a085a2
maks362880 <48396251+maks362880@users.noreply.github.com>
2023-01-17 12:06:40 +0300

    - minor updates

--- src/readAndWrite/BufferedReadFiles.java
+++ src/readAndWrite/BufferedReadFiles.java
@@ -2,6 +2,7 @@ package readAndWrite;
 
 import exeptionHandling.ExceptionAndLogFile;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -10,10 +11,13 @@ public class BufferedReadFiles {
 
     public void read(List<ModifiedBufferedReader> mBufferList, String inputFileName, int maxPartSizeFileKb) {
         try {
-            ModifiedBufferedReader mbr = new ModifiedBufferedReader(
-                    new FileReader("resource\\" + inputFileName), maxPartSizeFileKb * 1024);
-            mbr.setNameOfFile(inputFileName);
-            mBufferList.add(mbr);
+            File file = new File("resource\\" + inputFileName);
+            if (file.length() != 0L) {
+                ModifiedBufferedReader mbr = new ModifiedBufferedReader(
+                        new FileReader("resource\\" + inputFileName), maxPartSizeFileKb * 1024);
+                mbr.setNameOfFile(inputFileName);
+                mBufferList.add(mbr);
+            }
         } catch (IOException e) {
             new ExceptionAndLogFile("Something wrong in file '" + inputFileName +
                     "' error message: " + e.getMessage());
