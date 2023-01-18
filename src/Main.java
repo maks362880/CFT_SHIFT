5aa90f72a68280e38d408212274b95aa036877a9
maks362880 <48396251+maks362880@users.noreply.github.com>
2023-01-18 12:46:16 +0300

    -- minor changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import facade.MergeSortFacade;
 public class Main {
     public static void main(String[] args) {
         MergeSortFacade mergeSort = new MergeSortFacade();
-        mergeSort.setMaxPartSizeFileKb(1024);//не обязательный параметр
         mergeSort.start(args);
     }
 
